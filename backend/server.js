00d98da74703c666b91807c4da69bcc2fcc4438c
Sudhirkumar Kuchara <109846483+Sudhirkumar6009@users.noreply.github.com>
2025-12-15 15:53:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://libra-xpert.vercel.app",
+  "https://libraxpert.vercel.app",
   "https://libraxpert.onrender.com"
 ];
 
